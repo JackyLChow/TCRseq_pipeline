9e65b1d4fd13fe36991f40c8841d5afc4b0193e9
Jacky Chow <41753000+JackyLChow@users.noreply.github.com>
2023-12-04 10:08:17 -0500

    updates

--- R_code/TCRseq_differential_abundance.R
+++ R_code/TCRseq_differential_abundance.R
@@ -12,7 +12,7 @@ differential_clone_abundance_results_folder <- "~/Documents/BFX_proj/TCRseq_pipe
 comparison_matrix <- data.frame(sample_a = "P10-PB1.tsv",
                                 sample_b = unique(clone_data$file),
                                 id = "nucleic_acid",
-                                d_a_method = "binomial")
+                                d_a_method = "fisher")
 
 differential_clone_abundance_calculator <- function(
     clone_dat = clone_data,
@@ -27,6 +27,7 @@ differential_clone_abundance_calculator <- function(
     a_ <- comparison_mtx[i, "sample_a"]
     b_ <- comparison_mtx[i, "sample_b"]
     id_ <- comparison_mtx[i, "id"]
+    d_a_m_ <- comparison_mtx[i, "d_a_method"]
     
     counts_a <- clone_dat[clone_dat$file == a_, c(id_, "count")][1:10, ]
     if(any(duplicated(counts_a[, id_]))){
@@ -48,14 +49,21 @@ differential_clone_abundance_calculator <- function(
     # Differential clone abundance
     p_value <- c()
     for(j in 1:nrow(counts)){
-      p_value <- c(p_value,
-                   binom.test(counts[j, "count_a"], counts[j, "count_a"] + counts[j, "count_b"], p = sumA/(sumA + sumB), alternative = "two.sided")$p.value) # p_value for binomial test
+      if(d_a_m_ == "binomial"){
+        p_value <- c(p_value,
+                     binom.test(counts[j, "count_a"], counts[j, "count_a"] + counts[j, "count_b"], p = sumA/(sumA + sumB), alternative = "two.sided")$p.value) # p_value for binomial test
+      }
+      if(d_a_m_ == "fisher"){
+        p_value <- c(p_value,
+                     fisher.test(matrix(c(counts[j, "count_a"], sumA, counts[j, "count_b"], sumB), nrow = 2), alternative = "two.sided")$p.value)
+      }
     }
     
     differential_abundance <- data.frame(sample_a = a_, sample_b = b_,
                                          id = counts[, 1], counts[, c("count_a", "count_b")],
                                          p_value = p_value,
-                                         p_adj = p.adjust(p_value, method = "BH"))
+                                         p_adj = p.adjust(p_value, method = "BH"),
+                                         p_value_method = d_a_m_)
     differential_abundance$freq_a <- differential_abundance$count_a/sum(differential_abundance$count_a)
     differential_abundance$freq_b <- differential_abundance$count_b/sum(differential_abundance$count_b)
     differential_abundance$significance <- ifelse(
