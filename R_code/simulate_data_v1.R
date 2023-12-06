3cc27918bdae6f8e0b313dd1893f46b94b791fde
Jacky Chow <41753000+JackyLChow@users.noreply.github.com>
2023-12-06 11:06:43 -0500

    Update simulate_data_v1.R

--- R_code/simulate_data_v1.R
+++ R_code/simulate_data_v1.R
@@ -2,15 +2,22 @@
 
 ## simulated dataframe
 sim_tcr_data <- data.frame(clone = paste("clone_", 1:10000),
-                           near_perfectly_clonal = c(90001, rep(1, 9999)),
+                           perfectly_clonal = c(99999, rep(1/999, 9999)),
                            highly_clonal = c(40000, rep(60000/9999, 9999)),
                            oligoclonal = c(40000, 20000, 10000, rep(30000/9997, 9997)),
                            perfectly_diverse = rep(10, 10000))
 
-### Shannon ---
-p_ <- freq_/sum(freq_)
-p_ <- p_[p_ > 0]
-sh_en_ <- -sum(p_ * log2(p_)) # calculate shannon entropy
-sh_cl_ <- 1 - (sh_en_ / log2(length(freq_))) # calculate shannon clonality
-### Simpson ---
-si_cl_ <- sqrt(sum((freq_ / sum(freq_)) ^ 2))
+## evenness
+sample_names <- names(sim_tcr_data)[2:5]
+
+for(i in sample_names){
+  ### Shannon ---
+  freq_ <- sim_tcr_data[, i]/10000
+  p_ <- freq_/100000
+  p_ <- p_[p_ > 0]
+  sh_en_ <- -sum(p_ * log2(p_)) # calculate shannon entropy
+  sh_cl_ <- 1 - (sh_en_ / log2(10000)) # calculate shannon clonality
+  ### Simpson ---
+  si_cl_ <- sqrt(sum((freq_ / 100000) ^ 2))
+}
+
