78ff5f19ecd351b581bf510285482bdcbe48fadd
Jacky Chow <41753000+JackyLChow@users.noreply.github.com>
2024-01-05 08:04:06 -0500

    Update TCRseq_additional_diversity_parameter_calculator.R

--- R_code/TCRseq_additional_diversity_parameter_calculator.R
+++ R_code/TCRseq_additional_diversity_parameter_calculator.R
@@ -2,7 +2,7 @@ library(DescTools)
 
 # Additional diversity parameter calculator
 
-clone_data <- readRDS("~/Documents/BFX_proj/INT/processed_data/TCRseq/iRep_2023_12/munch_1_clone_data.rds")
+clone_data <- readRDS("")
 
 additional_diversity <- data.frame()
 
